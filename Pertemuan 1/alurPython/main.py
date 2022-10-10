b18b21e79d711b1dd5141214223e69d0910c5284
Muhammad Fauzan Abrar <=>
2022-10-10 21:57:52 +0800

    Add alurPyhton main.py and remove .pyc and pycache in gitignore

--- Pertemuan 1/alurPython/main.py
+++ Pertemuan 1/alurPython/main.py
@@ -1,1 +1,18 @@
+print("hello")
+print("world")
 print("hello world")
+
+# komentar di python
+"""
+    Multi line comment 
+    in 
+    python
+"""
+
+print("test")
+a = 10
+print(a)
+b = a + 5
+print(b)
+
+# bytecode adalah compile python
